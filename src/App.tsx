2037cdebc81687f241375e8e9ca4f894f20a8c0c
unknown <patelpreyansh3011@gmail.com>
2022-12-25 02:19:54 +0530

    add custom theme of mui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,35 @@ import "./App.css";
 import "./style/utility.css";
 import PreviewField from "./components/PreviewField";
 import InputField from "./components/InputField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          margin: "10px 5px",
+          width: "300px",
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          margin: "10px 5px",
+          width: "300px",
+          backgroundColor: "white",
+        },
+      },
+    },
+  },
+});
 
 function App() {
   const [jsonData, setJsonData] = useState("");
@@ -14,8 +43,10 @@ function App() {
 
   return (
     <div className="app">
-      <InputField handleOnChange={handleOnChange} jsonData={jsonData} />
-      <PreviewField jsonData={[jsonData]} />
+      <ThemeProvider theme={theme}>
+        <InputField handleOnChange={handleOnChange} jsonData={jsonData} />
+        <PreviewField jsonData={[jsonData]} />
+      </ThemeProvider>
     </div>
   );
 }
